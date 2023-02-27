99545d5576b5fb7d5c4bb3c5e1553ccf282e0119
eolsevski <e.olsevski@gmail.com>
2023-02-27 13:39:29 +0200

    Initial commit

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                switch (item.Name)
+                {
+                    case { } s when s.StartsWith("Sulfuras"):
+                        item.SellIn = 0;
+                        item.Quality = 80;
+                        break;
+                    case "Aged Brie":
+                        item.SellIn -= 1;
+                        item.Quality += 1;
+                        validate(item);
+                        break;
+                    case { } s when s.StartsWith("Backstage passes"):
+                        item.Quality += defineTicketStep(item.SellIn);
+                        item.SellIn -= 1;
+                        validate(item);
+                        if (item.SellIn == 0) item.Quality = 0;
+                        break;
+                    case { } s when s.StartsWith("Conjured"):
+                        item.Quality -= 1;
+                        standartLowering(item);
+                        break;
+                    default:
+                        standartLowering(item);
+                        break;
+                }
+            }
+        }
+
+        private int defineTicketStep(int sellIn)
+        {
+            var qualityStep = 1;
+            if (sellIn <= 10 && sellIn > 0) qualityStep += 1;
+            if (sellIn <= 5 && sellIn > 0) qualityStep += 1;
+            return qualityStep;
+        }
+
+        private void standartLowering(Item item)
+        {
+            if (item.SellIn == 0) item.Quality -= 1;
+            item.Quality -= 1;
+            item.SellIn -= 1;
+            validate(item);
+        }
+
+        private void validate(Item item)
+        {
+            if (item.SellIn < 0) item.SellIn = 0;
+            if (item.Quality < 0) item.Quality = 0;
+            if (item.Quality > 50) item.Quality = 50;
+        }
+    }
+}
---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,84 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests
+{
+    public class GildedRoseTest
+    {
+        [Theory]
+        [InlineData("foo",10,10,9,9)]
+        [InlineData("foo",0,10,0,8)]
+        [InlineData("foo",0,0,0,0)]
+        [InlineData("foo",-1,-1,0,0)]
+        [InlineData("foo",1,59,0,50)]
+        public void standartItemTests(string name, int actualSellin, int actualQuantity, int expectedSellInn, int expectedQuantity)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = actualSellin, Quality = actualQuantity } };
+
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(name, Items[0].Name);
+            Assert.Equal(expectedSellInn, Items[0].SellIn);
+            Assert.Equal(expectedQuantity, Items[0].Quality);
+        }
+        [Theory]
+        [InlineData("Sulfuras, Hand of Ragnaros", 10, 10, 0, 80)]
+        [InlineData("Sulfuras, Hand of Ragnaros", -1, 10, 0, 80)]
+       
+        public void legendaryItemTests(string name, int actualSellin, int actualQuantity, int expectedSellInn, int expectedQuantity)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = actualSellin, Quality = actualQuantity } };
+
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(name, Items[0].Name);
+            Assert.Equal(expectedSellInn, Items[0].SellIn);
+            Assert.Equal(expectedQuantity, Items[0].Quality);
+        }
+        [Theory]
+        [InlineData("Aged Brie", 10, 10, 9, 11)]
+        [InlineData("Aged Brie", 10, 50, 9, 50)]
+
+        public void agedBrieItemTests(string name, int actualSellin, int actualQuantity, int expectedSellInn, int expectedQuantity)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = actualSellin, Quality = actualQuantity } };
+
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(name, Items[0].Name);
+            Assert.Equal(expectedSellInn, Items[0].SellIn);
+            Assert.Equal(expectedQuantity, Items[0].Quality);
+        }
+        [Theory]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 11, 10, 10, 11)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 10, 10, 9, 12)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 5, 10, 4, 13)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 1, 10, 0, 0)]
+        
+        public void backstageItemTests(string name, int actualSellin, int actualQuantity, int expectedSellInn, int expectedQuantity)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = actualSellin, Quality = actualQuantity } };
+
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(name, Items[0].Name);
+            Assert.Equal(expectedSellInn, Items[0].SellIn);
+            Assert.Equal(expectedQuantity, Items[0].Quality);
+        }
+        [Theory]
+        [InlineData("Conjured Mana Cake", 10, 10, 9, 8)]
+        [InlineData("Conjured Mana Cake", 0, 0, 0, 0)]
+
+        public void conjuredItemTests(string name, int actualSellin, int actualQuantity, int expectedSellInn, int expectedQuantity)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = actualSellin, Quality = actualQuantity } };
+
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(name, Items[0].Name);
+            Assert.Equal(expectedSellInn, Items[0].SellIn);
+            Assert.Equal(expectedQuantity, Items[0].Quality);
+        }
+    }
+}
